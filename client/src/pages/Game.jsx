73df693e5896bc957dce5e002b7c4ef49f03d467
mohamedaminefezzani <fezzani.ami@gmail.com>
2026-06-16 23:29:43 +0100

    placeholder Game.jsx

---
+++ client/src/pages/Game.jsx
@@ -0,0 +1,7 @@
+export default function Game() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 100, fontFamily: 'sans-serif' }}>
+      <h2>Game coming soon...</h2>
+    </div>
+  )
+}
